ad8152f237a96a3e8ec73c743bbd7f299e490bb2
LJunsu <tpwotpwo13@gmail.com>
2025-05-05 15:38:48 +0900

    250505_2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,5 @@
 import type { Metadata } from "next";
-import { Roboto } from "next/font/google";
 import "./globals.css";
-
-const roboto = Roboto({
-  subsets: ["latin"],
-  weight: ["400", "500"],
-  style: ["normal", "italic"],
-  variable: "--roboto-text"
-})
 
 export const metadata: Metadata = {
   title: {
